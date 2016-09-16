672876b8452d814723f35467971056a2127dbf97
filipdbrsk <filip.dbrsk@gmail.com>
2016-09-16 16:00:57 +0200

    Works both for Python 2 and 3

---
+++ query_virtuoso_python2.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import urllib
+from requests.auth import HTTPDigestAuth
+
+username = ''
+password = ''
+server = 'http://dbpedia.org/sparql' # Replace this with your SPARQL instance
+
+def get_results(query):
+	auth = HTTPDigestAuth(username, password)
+	q = {'query': query, 'format': 'json'}
+	url = server + '?' + urllib.urlencode(q)
+	#print url
+	r = requests.get(url=url, auth=auth)  
+	page = r.content
+	return json.loads(page)["results"]["bindings"]
+
+query='SELECT * WHERE { ?a ?b ?c } LIMIT 10' # Here is your query
+for result in get_results(query):
+	print result["b"]["value"], result["c"]["value"] # Note that ?b and ?c are coming from the query. 
+	#If the variable names change in the query, they should change in this line too.
--- query_virtuoso_python3.py
+++ query_virtuoso_python3.py
@@ -5,18 +5,18 @@ from requests.auth import HTTPDigestAuth
 
 username = ''
 password = ''
-server = 'http://pvsge014.labs.vu.nl:8890/sparql' # Replace this with your SPARQL instance
+server = 'http://dbpedia.org/sparql' # Replace this with your SPARQL instance
 
 def get_results(query):
 	auth = HTTPDigestAuth(username, password)
 	q = {'query': query, 'format': 'json'}
-	url = server + '?' + urllib.urlencode(q)
+	url = server + '?' + urllib.parse.urlencode(q)
 	#print url
 	r = requests.get(url=url, auth=auth)  
-	page = r.content
-	return json.loads(page)["results"]["bindings"]
+	page = r.json()
+	return page["results"]["bindings"]
 
 query='SELECT * WHERE { ?a ?b ?c } LIMIT 10' # Here is your query
 for result in get_results(query):
-	print result["b"]["value"], result["c"]["value"] # Note that ?b and ?c are coming from the query. 
+	print(result["b"]["value"], result["c"]["value"]) # Note that ?b and ?c are coming from the query. 
 	#If the variable names change in the query, they should change in this line too.
